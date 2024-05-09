92debee3123211b4439b315628eeae9dbd9e312d
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2024-05-09 01:00:29 -0500

    added member role table to make rls stuff a bit easier

--- seed.ts
+++ seed.ts
@@ -7,7 +7,7 @@
 import { createSeedClient } from "@snaplet/seed"
 
 const main = async () => {
-	const seed = await createSeedClient({ dryRun: true })
+	const seed = await createSeedClient()
 
 	// Truncate all tables in the database
 	await seed.$resetDatabase()
