3e4f16dc2e652af98982aaf903bde0171dcf391c
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2024-06-01 01:17:21 -0500

    silly completion messages

--- components/ui/buttons/complete-interval-button.tsx
+++ components/ui/buttons/complete-interval-button.tsx
@@ -31,7 +31,39 @@ export function CompleteIntervalButton({ clubId, readingId, memberId, intervalId
 		},
 		onSuccess: () => {
 			if (!userProgress?.is_complete) {
-				toast.success("completed reading interval!")
+				const choices = [
+					`oooo you're on go mode! keep it up! 🔥`,
+					`nice one! was it a good interval?`,
+					`reading interval completed! 🎉`,
+					`justkeepreadingjustkeepreadingjustkeepreading`,
+					`#slayedthatinterval 💅`,
+					`i saw how hard you read that. nice.`,
+					`nice! if you read one more page i'll give you a dollar 😏`,
+					`breaking news: super reader on the loose! 🚨`,
+					`this just in: reader smashes interval! 📰`,
+					`oh. you didn't have to read it THAT fast 💨`,
+					`dang. i wish i could read like you 😔`,
+					`wow... way to show everyone how to actually read!`,
+					`you know you can take breaks... right?`,
+					`pfft... what a nerd! reading??? a book??? 🤣`,
+					`let me guess... you're on page ${Math.floor(Math.random() * 500)}. was i close?`,
+					`nice one. now go take a break and frolic in a field.`,
+					`interval completed! go drink some water now. you seem parched.`,
+					`beep boop. interval completed. beep boop. congratulations. beep boop.`,
+					`has anyone ever told you that you look like a cutie patootie when you read?`,
+					`omg same, i loved that part. totally. i've read the book like 20 times soooo`,
+					`it's exhausting coming up with these. sometimes i just want to say nice and be done with it, yaknow?`,
+					`people keep telling me it's weird i watch you read? anyways nice completion!`,
+					`when's the last time you did the macarena? just a thought. nice one!`,
+					`when's the last time you square danced? just a thought. nice one!`,
+					`nice! don't go too hard celebrating this completion! 🎉`,
+					`hmmm. very interesting part of the book. hmmm yes. indeed.`,
+					`is it just me or is it hotter in here since you completed the interval? 🥵`,
+					`SLUG BUG! trust me, i saw it right out my window. anyways nice completion!`,
+					`go reader! go reader! goooooo reader! 👏`,
+					`niiiiiiiiiiiiiiiiiiiiiiiice`,
+				]
+				toast.success(choices[Math.floor(Math.random() * choices.length)])
 			}
 			queryClient.invalidateQueries(["intervals", clubId, readingId])
 			queryClient.invalidateQueries(["user progress", intervalId])
