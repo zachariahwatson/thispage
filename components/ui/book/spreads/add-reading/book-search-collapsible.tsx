1b7d1dae96e69d1f924c907c29f768b28dfa0b84
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2024-08-04 19:56:38 -0500

    book search cover tweaks

--- components/ui/book/spreads/add-reading/book-search-collapsible.tsx
+++ components/ui/book/spreads/add-reading/book-search-collapsible.tsx
@@ -79,21 +79,40 @@ export function BookSearchCollapsible({ item, radioRef }: Props) {
 									: null}
 							</span>
 							<FormDescription className="flex flex-row items-center text-xs">
-								view {editions && editions.entries && editions.entries.length} editions{" "}
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									strokeWidth={1.5}
-									stroke="currentColor"
-									className="size-6"
-								>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
-									/>
-								</svg>
+								{loading ? (
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										strokeWidth={1.5}
+										stroke="currentColor"
+										className="size-4 animate-spin"
+									>
+										<path
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+										/>
+									</svg>
+								) : (
+									<>
+										view {editions && editions.entries && editions.entries.length} editions{" "}
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											strokeWidth={1.5}
+											stroke="currentColor"
+											className="size-6"
+										>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
+											/>
+										</svg>
+									</>
+								)}
 							</FormDescription>
 						</FormLabel>
 					</FormItem>
