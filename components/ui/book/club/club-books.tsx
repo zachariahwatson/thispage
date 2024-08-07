173fc5abe009d7dd6c0b6f300038fad982c86252
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2024-08-07 15:01:29 -0500

    maybe fixed infinite startup load?

--- components/ui/book/club/club-books.tsx
+++ components/ui/book/club/club-books.tsx
@@ -3,14 +3,15 @@
 import { ClubBook, ClubBookSkeleton } from "@/components/ui/book"
 import { ClubMembershipProvider } from "@/contexts"
 import { useClubs } from "@/hooks/state"
+import { useEffect } from "react"
 
 /**
  * returns a list of book clubs. we can't use suspense here as we need the fetch to be called from the client in order to preserve auth cookies. <Suspense> only works with async components and client components can't be async.
  */
 export function ClubBooks() {
-	const { data: clubMemberships, isLoading: loading } = useClubs()
+	const { data: clubMemberships, isSuccess: success } = useClubs()
 
-	return !loading && clubMemberships ? (
+	return success ? (
 		clubMemberships.length > 0 ? (
 			clubMemberships.map((clubMembership, index) => (
 				<ClubMembershipProvider key={index} clubMembershipData={clubMembership}>
