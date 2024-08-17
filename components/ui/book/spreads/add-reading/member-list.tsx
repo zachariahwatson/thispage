ae10acc363c082868c6f0f0ca2734b12bd941735
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2024-08-17 15:05:17 -0500

    dumb bug fixed

--- components/ui/book/spreads/add-reading/member-list.tsx
+++ components/ui/book/spreads/add-reading/member-list.tsx
@@ -23,6 +23,17 @@ import {
 import { Button } from "@/components/ui/buttons"
 import { toast } from "sonner"
 import { DataTableMembers } from "@/components/ui/data-table-members"
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+} from "@/components/ui/alert-dialog"
+import { useState } from "react"
 
 const defaultUrl = process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
 	? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
@@ -31,6 +42,8 @@ const defaultUrl = process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
 export function MemberList() {
 	const clubMembership = useClubMembership()
 	const queryClient = useQueryClient()
+	const [promoteVisible, setPromoteVisible] = useState<boolean>(false)
+	const [dropdownVisible, setDropdownVisible] = useState<boolean>(false)
 
 	const updateRoleMutation = useMutation({
 		mutationFn: (data: { editor_member_id: number; member_id: number; role: string }) => {
@@ -100,81 +113,96 @@ export function MemberList() {
 				return (
 					clubMembership?.role !== "member" &&
 					member.role !== "admin" && (
-						<DropdownMenu>
-							<DropdownMenuTrigger asChild>
-								<Button variant="ghost" className="p-2">
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										strokeWidth={1.5}
-										stroke="currentColor"
-										className="size-6"
-									>
-										<path
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-										/>
-									</svg>
-								</Button>
-							</DropdownMenuTrigger>
-							<DropdownMenuContent align="end">
-								{member.role !== "admin" && clubMembership?.role === "admin" ? (
-									<>
-										{member.role === "moderator" ? (
-											<>
+						<>
+							<DropdownMenu onOpenChange={setDropdownVisible}>
+								<DropdownMenuTrigger asChild>
+									<Button variant="ghost" className="p-2">
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											strokeWidth={1.5}
+											stroke="currentColor"
+											className="size-6"
+										>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+											/>
+										</svg>
+									</Button>
+								</DropdownMenuTrigger>
+								<DropdownMenuContent align="end">
+									{member.role !== "admin" && clubMembership?.role === "admin" ? (
+										<>
+											{member.role === "moderator" ? (
+												<>
+													<DropdownMenuItem className="cursor-pointer" onSelect={() => setPromoteVisible(true)}>
+														promote to admin
+													</DropdownMenuItem>
+													<DropdownMenuItem
+														className="cursor-pointer"
+														onSelect={() =>
+															updateRoleMutation.mutate({
+																editor_member_id: clubMembership.id,
+																member_id: member.id,
+																role: "member",
+															})
+														}
+													>
+														demote to member
+													</DropdownMenuItem>
+												</>
+											) : (
 												<DropdownMenuItem
 													className="cursor-pointer"
 													onSelect={() =>
 														updateRoleMutation.mutate({
 															editor_member_id: clubMembership.id,
 															member_id: member.id,
-															role: "admin",
+															role: "moderator",
 														})
 													}
 												>
-													promote to admin
+													promote to moderator
 												</DropdownMenuItem>
-												<DropdownMenuItem
-													className="cursor-pointer"
-													onSelect={() =>
-														updateRoleMutation.mutate({
-															editor_member_id: clubMembership.id,
-															member_id: member.id,
-															role: "member",
-														})
-													}
-												>
-													demote to member
-												</DropdownMenuItem>
-											</>
-										) : (
-											<DropdownMenuItem
-												className="cursor-pointer"
-												onSelect={() =>
-													updateRoleMutation.mutate({
-														editor_member_id: clubMembership.id,
-														member_id: member.id,
-														role: "moderator",
-													})
-												}
-											>
-												promote to moderator
-											</DropdownMenuItem>
-										)}
+											)}
 
-										<DropdownMenuSeparator />
-										<DropdownMenuItem className="text-destructive cursor-pointer">kick</DropdownMenuItem>
-									</>
-								) : (
-									member.role === "member" &&
-									clubMembership?.role === "moderator" && (
-										<DropdownMenuItem className="text-destructive cursor-pointer">kick</DropdownMenuItem>
-									)
-								)}
-							</DropdownMenuContent>
-						</DropdownMenu>
+											<DropdownMenuSeparator />
+											<DropdownMenuItem className="text-destructive cursor-pointer">kick</DropdownMenuItem>
+										</>
+									) : (
+										member.role === "member" &&
+										clubMembership?.role === "moderator" && (
+											<DropdownMenuItem className="text-destructive cursor-pointer">kick</DropdownMenuItem>
+										)
+									)}
+								</DropdownMenuContent>
+							</DropdownMenu>
+							<AlertDialog open={promoteVisible && !dropdownVisible} onOpenChange={setPromoteVisible}>
+								<AlertDialogContent>
+									<AlertDialogHeader>
+										<AlertDialogTitle>are you sure?</AlertDialogTitle>
+										<AlertDialogDescription>this action cannot be undone.</AlertDialogDescription>
+									</AlertDialogHeader>
+									<AlertDialogFooter>
+										<AlertDialogCancel>cancel</AlertDialogCancel>
+										<AlertDialogAction
+											onClick={() =>
+												updateRoleMutation.mutate({
+													editor_member_id: clubMembership?.id || -1,
+													member_id: member.id,
+													role: "admin",
+												})
+											}
+										>
+											promote
+										</AlertDialogAction>
+									</AlertDialogFooter>
+								</AlertDialogContent>
+							</AlertDialog>
+						</>
 					)
 				)
 			},
