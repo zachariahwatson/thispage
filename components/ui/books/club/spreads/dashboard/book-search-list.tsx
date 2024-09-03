6da80c3b544156c4454ca81bd482a70c211e8217
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2024-09-03 04:37:53 -0500

    remove scrollarea + language filter implementation

--- components/ui/books/club/spreads/dashboard/book-search-list.tsx
+++ components/ui/books/club/spreads/dashboard/book-search-list.tsx
@@ -1,18 +1,25 @@
 "use client"
-import { ScrollArea } from "@/components/ui"
 import { BookSearchCollapsible, BookSearchCollapsibleSkeleton } from "@/components/ui/books/club/spreads/dashboard"
 import { BookSearch } from "@/lib/types"
-import React, { MutableRefObject, useEffect } from "react"
+import React, { useEffect, useRef } from "react"
 import { useInView } from "react-intersection-observer"
 import { useInfiniteQuery } from "react-query"
 
 interface Props {
 	search: string
-	radioRef: MutableRefObject<HTMLDivElement | null>
+	groupValue: string | undefined
+	language:
+		| {
+				name: string
+				key: string
+				count: number
+		  }
+		| undefined
 }
 
-export function BookSearchList({ search, radioRef }: Props) {
+export function BookSearchList({ search, groupValue, language }: Props) {
 	const { ref, inView, entry } = useInView()
+	const scrollRef = useRef<HTMLDivElement>(null)
 
 	const defaultUrl = process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
 		? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
@@ -29,7 +36,9 @@ export function BookSearchList({ search, radioRef }: Props) {
 		queryKey: ["books", search],
 		queryFn: async ({ pageParam }) => {
 			if (search !== "") {
-				const url = new URL(`${defaultUrl}/api/books/${search.replaceAll(" ", "+")}?page=${pageParam}`)
+				const url = new URL(`${defaultUrl}/api/books/${search.replaceAll(" ", "+")}`)
+				url.searchParams.append("page", pageParam)
+				url.searchParams.append("language", language?.key.split("/")[2] ?? "eng")
 				const response = await fetch(url, {
 					method: "GET",
 					headers: {
@@ -56,51 +65,61 @@ export function BookSearchList({ search, radioRef }: Props) {
 	}, [search])
 
 	useEffect(() => {
+		refetch()
+		if (scrollRef.current) {
+			scrollRef.current.scrollTo({ top: 0, behavior: "smooth" })
+		}
+	}, [language])
+
+	useEffect(() => {
 		if (inView && hasNextPage) {
 			fetchNextPage()
 		}
 	}, [inView])
 
 	return (
-		<ScrollArea className="h-96 shadow-inner rounded-md shadow-shadow pt-3">
-			<div className="p-4 pt-0 space-y-3">
-				{books &&
-					books.pages.map((group: any, i: number) => (
-						<React.Fragment key={i}>
-							{group &&
-								group.docs?.map((book: any) => (
-									<BookSearchCollapsible item={book} key={book.key} radioRef={radioRef} />
-								))}
-						</React.Fragment>
-					))}
-				{loading && !loadingNext ? (
-					<>
-						<BookSearchCollapsibleSkeleton />
-						<BookSearchCollapsibleSkeleton />
-						<BookSearchCollapsibleSkeleton />
-						<BookSearchCollapsibleSkeleton />
-						<BookSearchCollapsibleSkeleton />
-					</>
-				) : loadingNext ? (
-					<div className="flex justify-center">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							strokeWidth={1.5}
-							stroke="currentColor"
-							className="size-6 animate-spin"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-							/>
-						</svg>
-					</div>
-				) : null}
-				<div ref={ref} />
-			</div>
-		</ScrollArea>
+		// <ScrollArea className="h-96 shadow-inner rounded-md shadow-shadow pt-3">
+		<div
+			ref={scrollRef}
+			className="p-4 pt-3 space-y-3 h-96 shadow-inner rounded-md shadow-shadow w-full overflow-y-scroll"
+		>
+			{books &&
+				books.pages.map((group: BookSearch, i: number) => (
+					<React.Fragment key={i}>
+						{group &&
+							group.docs?.map((book: BookSearch["docs"][number]) => (
+								<BookSearchCollapsible item={book} key={book.key} groupValue={groupValue} language={language} />
+							))}
+					</React.Fragment>
+				))}
+			{loading && !loadingNext ? (
+				<>
+					<BookSearchCollapsibleSkeleton />
+					<BookSearchCollapsibleSkeleton />
+					<BookSearchCollapsibleSkeleton />
+					<BookSearchCollapsibleSkeleton />
+					<BookSearchCollapsibleSkeleton />
+				</>
+			) : loadingNext ? (
+				<div className="flex justify-center">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						className="size-6 animate-spin"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+						/>
+					</svg>
+				</div>
+			) : null}
+			<div ref={ref} />
+		</div>
+		// </ScrollArea>
 	)
 }
