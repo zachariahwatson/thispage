be4e301df289c53db571278e95f4588d2c2690d9
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2024-09-09 18:17:53 -0500

    added error components

--- components/ui/books/club/spreads/spreads.tsx
+++ components/ui/books/club/spreads/spreads.tsx
@@ -4,11 +4,12 @@ import { Card, Separator, Skeleton } from "@/components/ui/"
 import { EmptyPageLeft, EmptyPageRight } from "@/components/ui/books/club/spreads"
 import { DashboardSpread } from "@/components/ui/books/club/spreads/dashboard"
 import { ReadingSpread } from "@/components/ui/books/club/spreads/reading"
-import { NextReading, Tabs } from "@/components/ui/buttons"
+import { Button, NextReading, Tabs } from "@/components/ui/buttons"
 import { PollProvider, ReadingProvider, useClubMembership } from "@/contexts"
 import { usePolls, useReadings, useSpreadsCount, useUserProgress } from "@/hooks/state"
 import { useEffect, useState } from "react"
 import { PollSpread } from "@/components/ui/books/club/spreads/poll"
+import { QueryError } from "@/utils/errors"
 
 /**
  * returns a list of book club reading "spreads". we can't use suspense here as we need the fetch to be called from the client in order to preserve auth cookies. <Suspense> only works with async components and client components can't be async.
@@ -16,10 +17,12 @@ import { PollSpread } from "@/components/ui/books/club/spreads/poll"
  */
 export function Spreads() {
 	const clubMembership = useClubMembership()
-	const { data: spreadsCount, isLoading: spreadsCountLoading } = useSpreadsCount(
-		clubMembership?.club.id || -1,
-		clubMembership?.role || "member"
-	)
+	const {
+		data: spreadsCount,
+		isLoading: spreadsCountLoading,
+		error: spreadsCountError,
+		refetch: spreadsCountRefetch,
+	} = useSpreadsCount(clubMembership?.club.id || -1, clubMembership?.role || "member")
 
 	const [userSpreadIndex, setUserSpreadIndex] = useState<number>(0)
 
@@ -34,59 +37,116 @@ export function Spreads() {
 
 	let spreadIndex = 0
 
-	const { data: readings, isLoading: readingsLoading } = useReadings(
-		clubMembership?.club.id || -1,
-		clubMembership?.id || -1
-	)
-	const { data: polls, isLoading: pollsLoading } = usePolls(clubMembership?.club.id || -1, clubMembership?.id || -1)
+	const {
+		data: readings,
+		isLoading: readingsLoading,
+		error: readingsError,
+		refetch: readingsRefetch,
+	} = useReadings(clubMembership?.club.id || -1, clubMembership?.id || -1)
+	const {
+		data: polls,
+		isLoading: pollsLoading,
+		error: pollsError,
+		refetch: pollsRefetch,
+	} = usePolls(clubMembership?.club.id || -1, clubMembership?.id || -1)
 
-	return readings && polls && spreadsCount ? (
-		<>
-			{readings.map(
-				(reading) =>
-					reading && (
-						<ReadingProvider key={spreadIndex} readingData={reading}>
-							<ReadingSpread isVisible={userSpreadIndex === spreadIndex} userSpreadIndex={spreadIndex++} />
-						</ReadingProvider>
+	return !spreadsCountError && !pollsError && !readingsError ? (
+		readings && polls && spreadsCount ? (
+			<>
+				{readings.map(
+					(reading) =>
+						reading && (
+							<ReadingProvider key={spreadIndex} readingData={reading}>
+								<ReadingSpread isVisible={userSpreadIndex === spreadIndex} userSpreadIndex={spreadIndex++} />
+							</ReadingProvider>
+						)
+				)}
+
+				{polls.map(
+					(poll) =>
+						poll && (
+							<PollProvider key={spreadIndex} pollData={poll}>
+								<PollSpread isVisible={userSpreadIndex === spreadIndex} userSpreadIndex={spreadIndex++} />
+							</PollProvider>
+						)
+				)}
+
+				{clubMembership?.role !== "member" ? (
+					<DashboardSpread
+						key={spreadIndex}
+						isVisible={userSpreadIndex === spreadIndex}
+						userSpreadIndex={spreadIndex++}
+						setUserSpreadIndex={setUserSpreadIndex}
+					/>
+				) : (
+					readings.length === 0 &&
+					polls.length === 0 && (
+						<div className="h-full flex flex-col md:flex-row rounded-lg bg-background">
+							<EmptyPageLeft userSpreadIndex={0} />
+							<EmptyPageRight userSpreadIndex={0} />
+						</div>
 					)
-			)}
+				)}
 
-			{polls.map(
-				(poll) =>
-					poll && (
-						<PollProvider key={spreadIndex} pollData={poll}>
-							<PollSpread isVisible={userSpreadIndex === spreadIndex} userSpreadIndex={spreadIndex++} />
-						</PollProvider>
-					)
-			)}
-
-			{clubMembership?.role !== "member" ? (
-				<DashboardSpread
-					key={spreadIndex}
-					isVisible={userSpreadIndex === spreadIndex}
-					userSpreadIndex={spreadIndex++}
+				<NextReading
+					userSpreadIndex={userSpreadIndex}
 					setUserSpreadIndex={setUserSpreadIndex}
+					len={spreadsCount?.total_spreads || spreadIndex}
 				/>
-			) : (
-				readings.length === 0 &&
-				polls.length === 0 && (
-					<div className="h-full flex flex-col md:flex-row rounded-lg bg-background">
-						<EmptyPageLeft userSpreadIndex={0} />
-						<EmptyPageRight userSpreadIndex={0} />
-					</div>
-				)
-			)}
 
-			<NextReading
-				userSpreadIndex={userSpreadIndex}
-				setUserSpreadIndex={setUserSpreadIndex}
-				len={spreadsCount?.total_spreads || spreadIndex}
-			/>
-
-			<Tabs spreadsCount={spreadsCount} userSpreadIndex={userSpreadIndex} setUserSpreadIndex={setUserSpreadIndex} />
-		</>
+				<Tabs spreadsCount={spreadsCount} userSpreadIndex={userSpreadIndex} setUserSpreadIndex={setUserSpreadIndex} />
+			</>
+		) : (
+			(pollsLoading || readingsLoading || spreadsCountLoading) && <SpreadSkeleton />
+		)
 	) : (
-		(pollsLoading || readingsLoading || spreadsCountLoading) && <SpreadSkeleton />
+		<div className="p-3 md:p-4 bg-destructive/15 flex flex-col justify-center items-center text-destructive space-y-2 rounded-md md:w-1/2 md:h-full mt-[calc(100%-2rem)] h-1/2 w-full md:ml-[50%] md:mt-0">
+			<div className="flex flex-row justify-center items-center w-full">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					strokeWidth={1.5}
+					stroke="currentColor"
+					className="size-16 mr-2"
+				>
+					<path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z"
+					/>
+				</svg>
+				<div>
+					{pollsError ? (
+						<>
+							<p>{(pollsError as QueryError).message}</p>
+							<p className="text-muted-foreground">{(pollsError as QueryError).code}</p>
+						</>
+					) : readingsError ? (
+						<>
+							<p>{(readingsError as QueryError).message}</p>
+							<p className="text-muted-foreground">{(readingsError as QueryError).code}</p>
+						</>
+					) : spreadsCountError ? (
+						<>
+							<p>{(spreadsCountError as QueryError).message}</p>
+							<p className="text-muted-foreground">{(spreadsCountError as QueryError).code}</p>
+						</>
+					) : null}
+				</div>
+			</div>
+			<Button
+				variant="secondary"
+				onClick={(e) => {
+					e.preventDefault()
+					pollsRefetch()
+					readingsRefetch()
+					spreadsCountRefetch()
+				}}
+			>
+				retry
+			</Button>
+		</div>
 	)
 }
 
--- components/ui/post/post.tsx
+++ components/ui/post/post.tsx
@@ -19,6 +19,7 @@ import { PostComments } from "@/components/ui/post"
 import { useMediaQuery } from "@/hooks"
 import { useClubs, useUser } from "@/hooks/state"
 import type { Post } from "@/lib/types"
+import { QueryError } from "@/utils/errors"
 import Image from "next/image"
 import Link from "next/link"
 import { redirect } from "next/navigation"
@@ -66,155 +67,173 @@ export function Post({ clubId, readingId, postId }: Props) {
 
 		if (!response.ok) {
 			const body = await response.json()
-			throw new Error(body.error)
+			throw new QueryError(body.message, body.code)
 		}
 
 		return await response.json()
 	}
 
-	const { data: post, isLoading: loading } = useQuery<Post>(["post", clubId, readingId, postId], () => fetchPost())
+	const {
+		data: post,
+		isLoading: loading,
+		error,
+		refetch,
+	} = useQuery<Post>(["post", clubId, readingId, postId], () => fetchPost())
 
 	const createdAt = post && new Date(post.created_at)
 
-	const isVertical = useMediaQuery("(max-width: 768px)")
-
-	const { data: coverImage } = useQuery<ProbeResult>({
-		queryKey: ["cover image", readingId],
-		queryFn: async () => {
-			const url = new URL(`${defaultUrl}/api/images?url=${post?.reading.book_cover_image_url}`)
-			const response = await fetch(url, {
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-				},
-			})
-
-			if (!response.ok) {
-				const body = await response.json()
-				throw new Error(body.error)
-			}
-
-			return await response.json()
-		},
-	})
-
-	return !loading && post ? (
-		<div className="flex flex-col justify-center max-w-4xl w-full space-y-4">
-			<div className="space-y-2">
-				<div className="flex flex-row items-center relative">
-					<Button className="absolute top-0 -left-16 rounded-full hidden lg:block" variant="ghost">
-						<Link href={`/#club-${clubId}`}>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								strokeWidth={1.5}
-								stroke="currentColor"
-								className="w-6 h-6"
-							>
-								<path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
-							</svg>
-						</Link>
-					</Button>
-					<div className="mr-4">
-						<Avatar className="w-8 h-8 md:w-10 md:h-10">
-							<AvatarImage src={post.member?.avatar_url || ""} />
-							<AvatarFallback>
-								{post.member?.first_name && post.member?.last_name
-									? post.member?.first_name[0] + post.member?.last_name[0]
-									: post.member?.name && post.member?.name.split(" ")[0][0] + post.member?.name.split(" ")[1][0]}
-							</AvatarFallback>
-						</Avatar>
-					</div>
-					<div className="relative max-w-[calc(100%-56px)] w-full">
-						<div className="flex flex-col pr-10">
-							<p className="text-md">
-								{post.member?.name} •{" "}
-								<span className="text-sm">
-									{createdAt
-										?.toLocaleDateString(undefined, {
-											year: "numeric",
-											month: "long",
-											day: "numeric",
-										})
-										.toLowerCase()}
-								</span>
-							</p>
-							<p className="text-muted-foreground italic truncate ... md:text-sm text-xs">
-								{post.reading.book_title} • {post.reading.club.name}
-							</p>
+	return !error ? (
+		!loading && post ? (
+			<div className="flex flex-col justify-center max-w-4xl w-full space-y-4">
+				<div className="space-y-2">
+					<div className="flex flex-row items-center relative">
+						<Button className="absolute top-0 -left-16 rounded-full hidden lg:block" variant="ghost">
+							<Link href={`/#club-${clubId}`}>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									strokeWidth={1.5}
+									stroke="currentColor"
+									className="w-6 h-6"
+								>
+									<path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+								</svg>
+							</Link>
+						</Button>
+						<div className="mr-4">
+							<Avatar className="w-8 h-8 md:w-10 md:h-10">
+								<AvatarImage src={post.member?.avatar_url || ""} />
+								<AvatarFallback>
+									{post.member?.first_name && post.member?.last_name
+										? post.member?.first_name[0] + post.member?.last_name[0]
+										: post.member?.name && post.member?.name.split(" ")[0][0] + post.member?.name.split(" ")[1][0]}
+								</AvatarFallback>
+							</Avatar>
 						</div>
-						<div className="absolute right-0 top-8">
-							<Sheet>
-								<SheetTrigger className="hover:ring-4 hover:ring-ring rounded transition-all">
-									<Image
-										className="rounded h-10 md:h-16 w-auto shadow-shadow shadow-md"
-										src={post.reading.book_cover_image_url || ""}
-										width={post.reading.book_cover_image_width || 0}
-										height={post.reading.book_cover_image_height || 0}
-										alt={
-											"Cover photo of " +
-											post.reading.book_title +
-											" by " +
-											(post.reading.book_authors?.length === 2
-												? post.reading.book_authors.join(" and ")
-												: post.reading.book_authors
-														?.map((author: string, i: number) => {
-															if (
-																i === (post.reading.book_authors ? post.reading.book_authors.length - 1 : 0) &&
-																post.reading.book_authors?.length !== 1
-															) {
-																return "and " + author
-															} else {
-																return author
-															}
-														})
-														.join(", "))
-										}
+						<div className="relative max-w-[calc(100%-56px)] w-full">
+							<div className="flex flex-col pr-10">
+								<p className="text-md">
+									{post.member?.name} •{" "}
+									<span className="text-sm">
+										{createdAt
+											?.toLocaleDateString(undefined, {
+												year: "numeric",
+												month: "long",
+												day: "numeric",
+											})
+											.toLowerCase()}
+									</span>
+								</p>
+								<p className="text-muted-foreground italic truncate ... md:text-sm text-xs">
+									{post.reading.book_title} • {post.reading.club.name}
+								</p>
+							</div>
+							<div className="absolute right-0 top-8">
+								<Sheet>
+									<SheetTrigger className="hover:ring-4 hover:ring-ring rounded transition-all">
+										<Image
+											className="rounded h-10 md:h-16 w-auto shadow-shadow shadow-md"
+											src={post.reading.book_cover_image_url || ""}
+											width={post.reading.book_cover_image_width || 0}
+											height={post.reading.book_cover_image_height || 0}
+											alt={
+												"Cover photo of " +
+												post.reading.book_title +
+												" by " +
+												(post.reading.book_authors?.length === 2
+													? post.reading.book_authors.join(" and ")
+													: post.reading.book_authors
+															?.map((author: string, i: number) => {
+																if (
+																	i === (post.reading.book_authors ? post.reading.book_authors.length - 1 : 0) &&
+																	post.reading.book_authors?.length !== 1
+																) {
+																	return "and " + author
+																} else {
+																	return author
+																}
+															})
+															.join(", "))
+											}
+										/>
+									</SheetTrigger>
+									<BookDetails
+										bookTitle={post.reading.book_title}
+										coverUrl={post.reading.book_cover_image_url || ""}
+										coverWidth={post.reading.book_cover_image_width || 0}
+										coverHeight={post.reading.book_cover_image_height || 0}
+										authors={post.reading.book_authors ?? undefined}
+										description={post.reading.book_description ?? undefined}
 									/>
-								</SheetTrigger>
-								<BookDetails
-									bookTitle={post.reading.book_title}
-									coverUrl={post.reading.book_cover_image_url || ""}
-									coverWidth={post.reading.book_cover_image_width || 0}
-									coverHeight={post.reading.book_cover_image_height || 0}
-									authors={post.reading.book_authors ?? undefined}
-									description={post.reading.book_description ?? undefined}
-								/>
-							</Sheet>
-						</div>
+								</Sheet>
+							</div>
 
-						{clubMembership && (clubMembership.role !== "member" || (!userLoading && user.id === post.member?.id)) && (
-							<PostActionsButton post={post} clubMembership={clubMembership} />
-						)}
+							{clubMembership &&
+								(clubMembership.role !== "member" || (!userLoading && user.id === post.member?.id)) && (
+									<PostActionsButton post={post} clubMembership={clubMembership} />
+								)}
+						</div>
 					</div>
+					<h1 className="text-lg md:text-2xl font-bold break-words">{post.title}</h1>
+					<p className="md:text-md text-sm break-words">{post.content}</p>
+					<LikeButton
+						likesCount={post.likes_count}
+						clubId={clubId}
+						readingId={readingId}
+						postId={postId}
+						memberId={String(clubMembership?.id)}
+					/>
 				</div>
-				<h1 className="text-lg md:text-2xl font-bold break-words">{post.title}</h1>
-				<p className="md:text-md text-sm break-words">{post.content}</p>
-				<LikeButton
-					likesCount={post.likes_count}
-					clubId={clubId}
-					readingId={readingId}
-					postId={postId}
-					memberId={String(clubMembership?.id)}
-				/>
+				<div className="pr-2">
+					<Separator />
+				</div>
+				<RootCommentButton clubId={clubId} readingId={readingId} postId={postId} memberId={memberId} />
+				{clubMembership && (
+					<PostComments
+						clubId={clubId}
+						readingId={readingId}
+						postId={postId}
+						memberId={memberId}
+						clubMembership={clubMembership}
+					/>
+				)}
 			</div>
-			<div className="pr-2">
-				<Separator />
-			</div>
-			<RootCommentButton clubId={clubId} readingId={readingId} postId={postId} memberId={memberId} />
-			{clubMembership && (
-				<PostComments
-					clubId={clubId}
-					readingId={readingId}
-					postId={postId}
-					memberId={memberId}
-					clubMembership={clubMembership}
-				/>
-			)}
-		</div>
+		) : (
+			<PostSkeleton />
+		)
 	) : (
-		<PostSkeleton />
+		<div className="p-3 md:p-4 flex flex-col justify-center items-center h-full text-destructive space-y-2">
+			<div className="flex flex-row justify-center items-center w-full">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					strokeWidth={1.5}
+					stroke="currentColor"
+					className="size-16 mr-2"
+				>
+					<path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z"
+					/>
+				</svg>
+				<div>
+					<p>{(error as QueryError).message}</p>
+					<p className="text-muted-foreground">{(error as QueryError).code}</p>
+				</div>
+			</div>
+			<Button
+				variant="secondary"
+				onClick={(e) => {
+					e.preventDefault()
+					refetch()
+				}}
+			>
+				retry
+			</Button>
+		</div>
 	)
 }
 
