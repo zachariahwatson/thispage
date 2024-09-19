aee75776b325b9de95af5dffc7277445f4608829
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2024-09-19 00:32:25 -0500

    add epilogue font

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import { createClient } from "@/utils/supabase/server"
 import { ClubBooks } from "@/components/ui/books/club"
-import { DemoSpreads } from "@/components/ui/demo"
+import { DemoSpreads } from "@/components/ui/books/demo"
 import { Card, WhatsNewDialog } from "@/components/ui"
 import Link from "next/link"
+import { FirstLoadAnimationProvider } from "@/contexts"
 
 export default async function Page() {
 	const supabase = createClient()
@@ -17,23 +18,23 @@ export default async function Page() {
 			<WhatsNewDialog />
 		</>
 	) : (
-		<div className="max-w-sm md:max-w-4xl w-full space-y-3">
-			<div>
-				<h1 className="font-bold text-lg md:text-3xl pl-1 truncate ...">
-					welcome to <span className="font-normal">this</span>
-					<span className="font-bold">page</span>!
-				</h1>
-				<h1 className="text-lg md:text-3xl pl-1 truncate ...">a simple site for book clubs.</h1>
-			</div>
-			<Card className="h-[calc(100svh-56px)] min-h-[624px] md:h-[624px] p-4 rounded-3xl relative shadow-shadow shadow-sm bg-book border-book-border">
+		<FirstLoadAnimationProvider key="demo">
+			<div className="max-w-sm md:max-w-4xl w-full space-y-3 relative">
+				<div>
+					<h1 className="font-bold text-lg md:text-3xl pl-1 truncate ... font-epilogue">
+						welcome to <span className="font-normal">this</span>
+						<span className="font-bold">page</span>!
+					</h1>
+					<h1 className="text-lg md:text-3xl pl-1 truncate ... font-epilogue">a simple site for book clubs.</h1>
+				</div>
 				<DemoSpreads />
-			</Card>
-			<h1 className="text-md md:text-2xl pl-1 truncate ... float-right">
-				<Link href="/login" className="underline text-primary">
-					login
-				</Link>{" "}
-				and start book clubbing!
-			</h1>
-		</div>
+				<h1 className="text-md md:text-2xl pl-1 truncate ... float-right font-epilogue">
+					<Link href="/login" className="underline text-primary">
+						login
+					</Link>{" "}
+					and start book clubbing!
+				</h1>
+			</div>
+		</FirstLoadAnimationProvider>
 	)
 }
