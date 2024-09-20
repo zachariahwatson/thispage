2f5615864be4f6b3c0dc625caefea7d97d8ca34b
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2024-09-20 17:52:17 -0500

    dynamic metadata

--- app/club/[clubId]/reading/[readingId]/comments/[postId]/page.tsx
+++ app/club/[clubId]/reading/[readingId]/comments/[postId]/page.tsx
@@ -1,5 +1,10 @@
+"use server"
+
 import { Post } from "@/components/ui/post"
-import { Metadata } from "next"
+import { Database } from "@/lib/types"
+import { QueryError } from "@/utils/errors"
+import { Metadata, ResolvingMetadata } from "next"
+import { useQuery } from "react-query"
 
 interface Props {
 	params: {
@@ -9,13 +14,69 @@ interface Props {
 	}
 }
 
-export const metadata: Metadata = {
-	title: "a post | thispage",
-	description: "join the discussion",
-	openGraph: {
-		title: "a post | thispage",
+// export const metadata: Metadata = {
+// 	title: "a post | thispage",
+// 	description: "join the discussion",
+// 	openGraph: {
+// 		title: "a post | thispage",
+// 		description: "join the discussion",
+// 	},
+// }
+
+const defaultUrl = process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
+	? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
+	: "http://localhost:3000"
+
+type PostType = Database["public"]["Views"]["post_metadata_view"]["Row"]
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+	//fetch post
+	const fetchPost = async () => {
+		const url = new URL(
+			`${defaultUrl}/api/clubs/${params.clubId}/readings/${params.readingId}/posts/${params.postId}/metadata`
+		)
+		const response = await fetch(url, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+
+		if (!response.ok) {
+			const body = await response.json()
+			throw new Error(body.error)
+		}
+
+		return await response.json()
+	}
+
+	// fetch data
+	const post: PostType = await fetchPost()
+
+	const imageAlt =
+		"Cover photo of " +
+		post.book_title +
+		" by " +
+		(post.book_authors?.length === 2
+			? post.book_authors.join(" and ")
+			: post.book_authors
+					?.map((author: string, i: number) => {
+						if (i === (post.book_authors ? post.book_authors.length - 1 : 0) && post.book_authors?.length !== 1) {
+							return "and " + author
+						} else {
+							return author
+						}
+					})
+					.join(", "))
+
+	return {
+		title: `${post.post_title} | ${post.book_title} in ${post.club_name} | thispage`,
 		description: "join the discussion",
-	},
+		openGraph: {
+			title: `${post.post_title} | ${post.book_title} | ${post.club_name} | thispage`,
+			description: "join the discussion",
+		},
+	}
 }
 
 export default async function Page({ params }: Props) {
