bf2be2770d46624a269eac316b7a4eb76fd39ac3
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2024-09-24 16:20:09 -0500

    remove props

--- app/reset/page.tsx
+++ app/reset/page.tsx
@@ -1,10 +1,6 @@
 import { ResetPage } from "@/components/ui/reset"
 import { Metadata } from "next"
 
-interface Props {
-	searchParams: { message?: string; type?: string; redirect: string }
-}
-
 export const metadata: Metadata = {
 	title: "reset password | thispage",
 	openGraph: {
@@ -12,6 +8,6 @@ export const metadata: Metadata = {
 	},
 }
 
-export default function Reset({ searchParams }: Props) {
-	return <ResetPage searchParams={searchParams} />
+export default function Reset() {
+	return <ResetPage />
 }
